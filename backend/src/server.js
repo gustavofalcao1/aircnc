52e063bd3e233a953bab95e41bcecf7890b8c07c
gustavofalcao1 <gustavo.falcao01@gmail.com>
2019-10-07 17:01:11 +0100

    Version 1.0

---
+++ backend/src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const routes = require('./routes');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-9-lpdeo.mongodb.net/semana09?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+})
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+server.listen(3333);
